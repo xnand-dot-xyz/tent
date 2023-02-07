dfa2a38e7fd8878a6e05777fc16b336d4d7e6272
Sunny <roesch.eric@protonmail.com>
2023-02-07 20:30:23 +0100

    Add introduction

--- index.php
+++ index.php
@@ -1,3 +1,36 @@
 <?php include "elements/header.php" ?>
 
+<h1>Tent</h1>
+<p>
+  Tent is a simple alternative front-end for Bandcamp.
+  <br>
+  It was inspired by Nitter and the like.
+</p>
+
+<h2>How do I get started?</h2>
+<p>Use the search bar at the top to find what you're looking for.</p>
+
+<h2>Why would I use this?</h2>
+<p>You might prefer Tent over the official Bandcamp website if you want to</p>
+<ul>
+  <li>browse Bandcamp without enabling JavaScript,</li>
+  <li>escape Bandcamp's analytics or</li>
+  <li>use a more lightweight website.</li>
+</ul>
+
+<h2>Why would I not use this?</h2>
+<p>The official website may be preferable if you want to</p>
+<ul>
+  <li>support the artists you listen to,</li>
+  <li>purchase releases or merchandise or</li>
+  <li>manage your account or collection.</li>
+</ul>
+
+<h2>Is Tent open-source?</h2>
+<p>
+  Tent is still in early development.
+  <br>
+  Once it is relatively mature, its source code will be released on Codeberg.
+</p>
+
 <?php include "elements/footer.php" ?>
