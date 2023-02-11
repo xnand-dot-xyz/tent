47bb778a1aeebb134947892822b10873a65e22fb
Sunny <roesch.eric@protonmail.com>
2023-02-11 11:48:52 +0100

    Show commit in footer

--- config/config.php
+++ config/config.php
@@ -1,12 +1,21 @@
 <?php
+  $commit = trim(file_get_contents("../.git/refs/heads/main"));
+
+  $text = substr($commit, 0, 7);
+  $link = "https://codeberg.org/sun/Tent/commit/" . $commit;
+
   $config = [
     // The title of this instance, displayed throughout the front-end. Default: Tent.
     "title" => "Tent",
 
     // A line of text displayed in the footer. May contain HTML. No default.
-    "text" => "© 2023-present, Sunny. Not affiliated with Bandcamp. Open-source at <a href=\"https://codeberg.org/sun/Tent\">Codeberg</a>.",
+    "text" => "© 2023-present, Sunny. Not affiliated with Bandcamp. Running commit <a href=\"" . $link . "\">" . $text . "</a>.",
 
     // Contents of the identity cookie when logged in to Bandcamp. Allows access to higher quality audio files. No default.
     "identity" => null
   ];
+
+  unset($commit);
+  unset($text);
+  unset($link);
 ?>
