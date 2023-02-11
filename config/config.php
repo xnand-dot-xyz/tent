348785dd5db7fe153e6bcd91e02922efe6c9ebf8
Sunny <roesch.eric@protonmail.com>
2023-02-11 11:15:18 +0100

    Add configuration documentation

--- config/config.php
+++ config/config.php
@@ -1,6 +1,12 @@
 <?php
   $config = [
+    // The title of this instance, displayed throughout the front-end. Default: Tent.
     "title" => "Tent",
-    "text" => "© 2023-present, Sunny. Not affiliated with Bandcamp. Open-source at <a href=\"https://codeberg.org/sun/Tent\">Codeberg</a>."
+
+    // A line of text displayed in the footer. May contain HTML. No default.
+    "text" => "© 2023-present, Sunny. Not affiliated with Bandcamp. Open-source at <a href=\"https://codeberg.org/sun/Tent\">Codeberg</a>.",
+
+    // Contents of the identity cookie when logged in to Bandcamp. Allows access to higher quality audio files. No default.
+    "identity" => null
   ];
 ?>
