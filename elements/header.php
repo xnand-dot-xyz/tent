41e8b80e4ec69ccc6889acced26e1910fc43d82c
Sunny <roesch.eric@protonmail.com>
2023-03-14 12:21:28 +0100

    Add logo by @SecularSteve, fixes #9

--- elements/header.php
+++ elements/header.php
@@ -16,12 +16,15 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?= $title ?></title>
+    <link rel="icon" href="asset.php?file=icon.svg">
     <link rel="stylesheet" href="asset.php?file=style.css">
   </head>
   <body>
     <header>
       <div class="wrapper">
-        <a href="."><?= $config["title"] ?></a>
+        <a href=".">
+          <img id="icon" src="asset.php?file=icon.svg">
+        </a>
         <form action="search.php">
           <input name="query" placeholder="Search..." required>
         </form>
