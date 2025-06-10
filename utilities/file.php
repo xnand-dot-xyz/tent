758a981a282ad4b6f2aa88c1d7926c937261872b
Sunny <sunny@sny.sh>
2025-06-10 08:07:41 +0200

    Improve condition formatting

--- utilities/file.php
+++ utilities/file.php
@@ -14,8 +14,10 @@
   function get_placeholder() {
     if (
       isset($_COOKIE["images"]) &&
-      ($_COOKIE["images"] === "disabled" ||
-      ($_COOKIE["images"] !== "enabled" && (isset($_SERVER["HTTP_SAVE_DATA"]) && $_SERVER["HTTP_SAVE_DATA"] === "on")))
+      (
+        $_COOKIE["images"] === "disabled" ||
+        ($_COOKIE["images"] !== "enabled" && (isset($_SERVER["HTTP_SAVE_DATA"]) && $_SERVER["HTTP_SAVE_DATA"] === "on"))
+      )
     ) {
       // Base64-encoded gray (50% opacity) PNG pixel
       return "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNsqAcAAYUBAdpOiIkAAAAASUVORK5CYII=";
