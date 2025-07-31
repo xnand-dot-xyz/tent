ff073b9ce582b4f231f215178f10ba41899d8aec
Sunny <sunny@sny.sh>
2025-07-31 21:56:53 +0200

    Fix inconsistent formatting

--- utilities/polyfill.php
+++ utilities/polyfill.php
@@ -1,11 +1,11 @@
 <?php
-  if (!function_exists('str_starts_with')) {
+  if (!function_exists("str_starts_with")) {
     function str_starts_with($haystack, $needle) {
       return substr($haystack, 0, strlen($needle)) === $needle;
     };
   };
 
-  if (!function_exists('str_ends_with')) {
+  if (!function_exists("str_ends_with")) {
     function str_ends_with($haystack, $needle) {
       return substr($haystack, -strlen($needle)) === $needle;
     };
